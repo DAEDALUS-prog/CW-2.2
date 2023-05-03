0851b680e7bc3acfcea1ef24e7682b1364ba6b19
Tim <tdubinchik3@gmail.com>
2023-05-04 02:49:31 +0300

    sq

--- app/calories/cls/models.py
+++ app/calories/cls/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    email = models.EmailField(max_length=30)
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=100)
+    status = models.IntegerField()
+    
+    
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    ccal = models.FloatField()
+    fat = models.FloatField()
+    proteins = models.FloatField()
+    carbohydrates = models.FloatField()
+    image = models.ImageField()
+    
+
+class History(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    desciption = models.TextField(max_length=150)
+    
